324ecaa292e499c422b3ab4ad9caaee5b4f67f12
Denis Surujiu <surujiudenis254@gmail.com>
2023-12-21 11:40:15 +0200

    Написал функцию для оттображения всех записей

--- 8.py
+++ 8.py
@@ -1,12 +1,19 @@
 
 def add(file_path):
-    name = input(":")
-    surname = input(":")
-    Email = input(":")
-    Phone = input(":")
+    name = input("Введите имя:")
+    surname = input("Введите фамилию:")
+    Email = input("Введите почту:")
+    Phone = input("Введите номер телефона:")
 
     with open(file_path, "a") as file:
         file.write(f"{surname}, {name}, {Email}, {Phone}n")
+
+def load(file_path):
+    with open(file_path, "r") as file:
+        for line in file:
+            print(line.strip())
+
+
 
 
 
